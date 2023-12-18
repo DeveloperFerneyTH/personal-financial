53d556fe05f58e41ddedb1ef519ee0090c35571b
ftalero_meli <ferney.talero@mercadolibre.com.co>
2023-12-17 20:30:28 -0500

    Movement form

--- src/views/pages/SamplePage.vue
+++ src/views/pages/SamplePage.vue
@@ -5,8 +5,30 @@ import UiParentCard from '@/components/shared/UiParentCard.vue';
 <template>
     <v-row>
         <v-col cols="12" md="12">
-            <UiParentCard title="Sample Page"> 
-                <div class=""><p class="text-body-1">This is a sample page </p></div>
+            <UiParentCard title="Movimientos"> 
+                <div class=""><p class="text-body-1">Adiciona un movimiento </p></div>
+                <v-form>
+                    <v-text-field
+                    label="Descripción"
+                    prepend-inner-icon="mdi-sticker-text"
+                    ></v-text-field>
+                    <v-text-field
+                    label="Costo"
+                    type="number"
+                    prepend-inner-icon="mdi-cash-multiple"
+                    ></v-text-field>
+                    <v-text-field
+                    label="Fecha"
+                    type="date"
+                    prepend-inner-icon="mdi-gift-open"
+                    ></v-text-field>
+                    <v-btn
+                    color="primary"
+                    type="submit" block
+                    class="mt-2"
+                    append-icon="mdi-plus-circle"
+                    >Crear</v-btn>
+                </v-form>
             </UiParentCard>
         </v-col>
     </v-row>
